9065fe64129da29882f47e748c527f4b2cabf685
Greg Patton <greg.patton@hp.com>
2013-05-30 21:35:26 -0500

    Simple XSS Secure Coding Demo
    XSS is preventable with secure coding practices.
    Validate Input!!
    Encode Untrusted Output!!!!

---
+++ XSS-SimpleCodeDefenseDemo/XSS.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="XSS.aspx.cs" ValidateRequest="false" Inherits="XSS_SimpleCodeDefenseDemo.XSS" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="Label1" runat="server" Text="Input Data:"></asp:Label>
+        <asp:TextBox ID="txtInput" runat="server"></asp:TextBox>
+        <asp:Button ID="cmdBad" runat="server" Text="Bad" onclick="cmdBad_Click" />
+        <asp:Button ID="cmdBetter" runat="server" Text="Better" 
+            onclick="cmdBetter_Click" />
+        <asp:Button ID="cmdBest" runat="server" Text="Best" onclick="cmdBest_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ XSS-SimpleCodeDefenseDemo/XSS.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace XSS_SimpleCodeDefenseDemo
+{
+    public partial class XSS : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e) {
+        }
+
+        protected void cmdBad_Click(object sender, EventArgs e) {
+            Response.Write("The input provided was " + txtInput.Text);
+        }
+
+        protected void cmdBetter_Click(object sender, EventArgs e) {
+            Response.Write("The input provided was " + Server.HtmlEncode(txtInput.Text));
+        }
+
+        protected void cmdBest_Click(object sender, EventArgs e) {
+            string pattern = @"\(?(\d{3})\)?-?(\d{3})-(\d{4})";
+            Regex r = new Regex(pattern, RegexOptions.IgnoreCase);
+            Match m = r.Match(txtInput.Text);
+            if (m.Success) {
+                Response.Write("The input provided was " + Server.HtmlEncode(txtInput.Text));
+            }
+            else {
+                Response.Write("The input provided was invalid.  Please provide a valid phone number.");
+            }
+        }
+    }
+}
